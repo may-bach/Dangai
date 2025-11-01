e1753164402df42d9f77a28c6954e5755651d8a8
izumi <detonater9841@gmail.com>
2025-11-01 21:30:05 +0530

    2400x1350 fix

--- Dangai Web/src/components/DecorativeLine.tsx
+++ Dangai Web/src/components/DecorativeLine.tsx
@@ -5,7 +5,7 @@ interface DecorativeLineProps {
 }
 
 const DecorativeLine: React.FC<DecorativeLineProps> = ({ variant = 'main' }) => {
-  const mainLineClasses = "w-full lg:w-[150%] lg:left-[-25%] h-[4px] my-14 md:my-16 shadow-[0_0_20px_rgba(255,255,255,0.4),0_0_40px_rgba(255,255,255,0.2)]";
+  const mainLineClasses = "w-full lg:w-[150%] 2xl:w-[125%] lg:left-[-25%] 2xl:left-[-12.5%] h-[4px] my-14 md:my-16 shadow-[0_0_20px_rgba(255,255,255,0.4),0_0_40px_rgba(255,255,255,0.2)]";
   const mainAfterClasses = "top-[-1px] w-[60%] h-[6px] opacity-60 shadow-[0_0_15px_rgba(255,255,255,0.3)]";
 
   const arcLineClasses = "w-full lg:w-[110%] lg:left-[-5%] h-[3px] my-10 md:my-12 shadow-[0_0_15px_rgba(255,255,255,0.3),0_0_30px_rgba(255,255,255,0.1)]";
